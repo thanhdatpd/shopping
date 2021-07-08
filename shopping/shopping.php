4e3091bc1fc429df6bf8742997306f90486ef130
alex nguyen <thanhdatppro@gmail.com>
2021-07-08 19:21:26 +0700

    a

--- shopping/shopping.php
+++ shopping/shopping.php
@@ -204,8 +204,8 @@
                 </div>
                 <div class="row">
                     <div class="col-lg-4 col-md-6 col-sm-6">
-                        <a href='/shopping?details=23424'>
-                            <div class="product__item">
+                        <div class="product__item">
+                            <a href='/shopping?details=123'>
                                 <div class="product__item__pic set-bg" data-setbg="/img/product/product-2.jpg">
                                     <ul class="product__hover">
                                         <li><a href="#"><img src="/img/icon/heart.png" alt=""></a></li>
@@ -215,32 +215,33 @@
                                         <li><a href="#"><img src="/img/icon/search.png" alt=""></a></li>
                                     </ul>
                                 </div>
-                                <div class="product__item__text">
-                                    <h6>Piqué Biker Jacket</h6>
-                                    <a href="#" class="add-cart">+ Add To Cart</a>
-                                    <div class="rating">
-                                        <i class="fa fa-star-o"></i>
-                                        <i class="fa fa-star-o"></i>
-                                        <i class="fa fa-star-o"></i>
-                                        <i class="fa fa-star-o"></i>
-                                        <i class="fa fa-star-o"></i>
-                                    </div>
-                                    <h5>$67.24</h5>
-                                    <div class="product__color__select">
-                                        <label for="pc-4">
-                                            <input type="radio" id="pc-4">
-                                        </label>
-                                        <label class="active black" for="pc-5">
-                                            <input type="radio" id="pc-5">
-                                        </label>
-                                        <label class="grey" for="pc-6">
-                                            <input type="radio" id="pc-6">
-                                        </label>
-                                    </div>
+                            </a>
+
+                            <div class="product__item__text">
+                                <h6>Piqué Biker Jacket</h6>
+                                <a href="#" class="add-cart">+ Add To Cart</a>
+                                <div class="rating">
+                                    <i class="fa fa-star-o"></i>
+                                    <i class="fa fa-star-o"></i>
+                                    <i class="fa fa-star-o"></i>
+                                    <i class="fa fa-star-o"></i>
+                                    <i class="fa fa-star-o"></i>
+                                </div>
+                                <h5>$67.24</h5>
+                                <div class="product__color__select">
+                                    <label for="pc-4">
+                                        <input type="radio" id="pc-4">
+                                    </label>
+                                    <label class="active black" for="pc-5">
+                                        <input type="radio" id="pc-5">
+                                    </label>
+                                    <label class="grey" for="pc-6">
+                                        <input type="radio" id="pc-6">
+                                    </label>
                                 </div>
                             </div>
-                        </a>
 
+                        </div>
                     </div>
                     <div class="col-lg-4 col-md-6 col-sm-6">
                         <div class="product__item sale">
